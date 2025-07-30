0deb1ad4d3a607dc634ea1cb83ef5633f04321aa
sakshamgiri202 <sakshamgiri500@gmail.com>
2025-07-30 22:21:46 +0530

    add basicfunction

--- 02BascisOperation/basicfuntion.py
+++ 02BascisOperation/basicfuntion.py
@@ -1,12 +1,13 @@
 import cv2 as cv
 
-img = cv.imread('/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg')
+img = cv.imread("/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg")
 
-cv.imshow('cat', img)
+cv.imshow("cat", img)
 
 # convert to grayscale
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-cv.imshow('gray', gray)
+cv.imshow("gray", gray)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+
