df692db5b475eeb1e79f44e4d208ea1f2d514b6e
sakshamgiri202 <sakshamgiri500@gmail.com>
2025-08-01 01:00:29 +0530

    made some changes readimage.py

--- 01Basics/readImage.py
+++ 01Basics/readImage.py
@@ -2,8 +2,8 @@ import cv2 as cv
 
 
 # Thsi finds and Load image from file path
-# img_path = '/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg'
-img = cv.imread('/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg')
+img_path = '/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg'
+img = cv.imread(img_path)
 
 # Display image window named 'Cat'
 cv.imshow('Cat', img)
