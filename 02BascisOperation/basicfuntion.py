b740f8dfdd9c5a63a66a6956104cb01151db3c78
sakshamgiri202 <sakshamgiri500@gmail.com>
2025-07-31 23:30:50 +0530

    make some changes in basicfunction and draw

--- 02BascisOperation/basicfuntion.py
+++ 02BascisOperation/basicfuntion.py
@@ -2,12 +2,38 @@ import cv2 as cv
 
 img = cv.imread("/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg")
 
-cv.imshow("cat", img)
+# cv.imshow("Cat", img)
 
 # convert to grayscale
-gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-cv.imshow("gray", gray)
+# gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+# cv.imshow("gray", gray)
+
+# blur
+blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
+cv.imshow("Blur", blur)
+
+# edge cascade
+canny = cv.Canny(blur, 125, 75)
+cv.imshow("canny edge", canny)
+
+# dilated the image
+dilated = cv.dilate(canny, (7, 7), iterations=3)
+cv.imshow("dilated", dilated)
+
+# eroding
+eroded = cv.erode(dilated, (3, 3), iteration=3)
+cv.imshow("eroded", eroded)
+
+# resize
+
+resized = cv.resized(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow("resized", resized)
+
+# cropped
+
+cropped = img[50:200, 200:400]
+cv.imshow("cropped", cropped)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
-
--- 02BascisOperation/draw.py
+++ 02BascisOperation/draw.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((500,500, 3), dtype="uint8")
-# cv.imshow('blank', blank)
+blank = np.zeros((500, 500, 3), dtype="uint8")
+cv.imshow("blank", blank)
 
 # 1. paint the image a certain color
 # blank[200:300, 300:400]=0,0,255
@@ -16,7 +16,7 @@ blank = np.zeros((500,500, 3), dtype="uint8")
 # cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2), 40, (0,255,0), thickness=-1)
 # cv.imshow('circle', blank)
 
-#  5. draw a line 
+#  5. draw a line
 # cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3),
 # cv.imshow('line', blank)
 
@@ -25,10 +25,9 @@ blank = np.zeros((500,500, 3), dtype="uint8")
 # cv.imshow("text", blank)
 
 
-
-
 # img = cv.imread('/home/shaggy/Public/opencv-playlist/Resources/Photos/cat.jpg')
 # cv.imshow('cat', img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+
